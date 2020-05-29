9318d1bb66b30b5643cafc243ebb98528b6f9e10
lgarciaee <l.garciae@uniandes.edu.co>
2020-05-29 19:46:45 -0400

    factorial

---
+++ factorial/.ipynb_checkpoints/main-checkpoint.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    
+    cout << "Hola mundo!\n Cual es tu nombre?" << endl;
+    int n;
+    string nombre;
+    cin >> nombre;
+    cout << "Hola" << nombre << endl;
+    cout  << "Tu edad es" << n << "anios" << endl;
+}
